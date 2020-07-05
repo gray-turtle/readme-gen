3d67ef2c4e4d477f551b45220ea125c785b679d1
Keith Fike <fkeithr@gmail.com>
2020-07-05 14:54:49 -0600

    added array of questions

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+const inquirer = require('inquirer');
+
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Add a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation instructions',
+    message: 'Add installation instructions:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Add usage information:'
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Add contribution guidlines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Add test instructions'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please select a license from the following list',
+    choices: ['MIT', 'GPL', 'Apache']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?'
+  }
 ];
 
 // function to write README file
